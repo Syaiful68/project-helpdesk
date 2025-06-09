206bfdde4ab566a0530375d7c06b2772a7b29da6
Syaiful68 <syaifulSheva@gmail.com>
2025-06-09 17:03:40 +0700

    create menu item

---
+++ src/components/partials/itemMenu.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  menu: Object,
+});
+</script>
+<template>
+  <li v-for="(item, index) in menu" class="nav-item">
+    <router-link class="nav-link" :to="item.path">
+      <span class="nav-link-icon d-md-none d-lg-inline-block"
+        ><!-- Download SVG icon from http://tabler.io/icons/icon/home -->
+        <i :class="item.icon"></i>
+      </span>
+      <span class="nav-link-title"> {{ item.label }} </span>
+    </router-link>
+  </li>
+</template>
